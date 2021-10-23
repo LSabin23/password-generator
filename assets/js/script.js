e83663b0186c505b71e2e59a3eaa40e99fbc27f4
unknown <44147809+LSabin23@users.noreply.github.com>
2021-10-23 11:18:46 -0400

    prompt for number of chars and validate

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,26 +3,52 @@ var generateBtn = document.querySelector('#generate')
 
 // Write password to the #password input
 function writePassword () {
-  var password = generatePassword() {
-    // length prompt: 8 to 128 characters, validate input
-
-
-    // character type prompt: uppercase
-
-
-    // character type prompt: lowercase
-
-
-    // character type prompt: numeric
-
-
-    // character type prompt: special
-
-
-  }
+  var password = generatePassword()
 
   var passwordText = document.querySelector('#password')
   passwordText.value = password
+
+}
+
+function generatePassword () {
+  // length prompt: 8 to 128 characters, validate input
+  var charPrompt = window.prompt('Enter a value for number of password characters (from 8 to 128).')
+
+  if (charPrompt < 8 || charPrompt > 128 || isNaN(charPrompt)) {
+    window.alert('Please enter a number from 8 to 128.')
+    return generatePassword()
+  }
+  else {
+    // use charCount value to set length of password string
+  }
+
+  // character type prompt: uppercase
+  var confirmUpper = window.confirm('Include uppercase characters?')
+
+  if (confirmUpper) {
+    // allow uppercase letters in password string
+  }
+
+  // character type prompt: lowercase
+  var confirmLower = window.confirm('Include lowercase characters?')
+
+  if (confirmLower) {
+    // allow lowercase letters in password string
+  }
+
+  // character type prompt: numeric
+  var confirmNumeric = window.confirm('Include numeric characters?')
+
+  if (confirmNumeric) {
+    // allow numeric characters in password string
+  }
+
+  // character type prompt: special
+  var confirmSpecial = window.confirm('Include special characters?')
+
+  if (confirmSpecial) {
+    // allow special characters in password string
+  }
 
 }
 
